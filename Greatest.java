55b9a325535b48244261e1628bfee223a7d34994
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-07-27 10:00:15 +0530

    Greatest.java

---
+++ Greatest.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Greatest
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int a=2,b=7,c=10;	
+	  if(a>b&&a>c)
+	  {
+	  	System.out.println("a is greatest");
+	  }
+	  	else if(b>c&&b>a)
+	  	{
+	  		System.out.println("b is greatest");
+	  	}
+	  	else
+	  	{
+	  		System.out.println("c is greatest");
+	  }
+	}
+}
