6d64d4a06825681e8ea30a0b4bfcd56cd7f95887
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-07-27 09:52:42 +0530

    Alphabate.java

---
+++ Alphabate.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Alphabate
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch='a';
+		if(ch=='a')
+		{
+			System.out.println(ch+ "is an alphabate");
+		}
+		else
+		{
+			System.out.println(ch+ "is a number");
+		}
+	}
+}
