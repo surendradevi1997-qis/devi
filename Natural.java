850886befc0a1e04635a9d01b2f3930cb774912d
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-07-27 10:23:59 +0530

    Natural.java

---
+++ Natural.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Natural
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int sum=0,i,n=10;
+		for(i=1;i<=n;i++)
+		{
+		sum=sum+i;
+		}
+	System.out.println("sum=" +sum);
+	}
+}
