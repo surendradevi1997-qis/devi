9f225dc6919b99fcec1e2540c5d3937ba150b29f
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-07-27 10:11:41 +0530

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Hello
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int i,n=2;
+	  for(i=1;i<=n;i++)
+	  {
+	  	System.out.println("Hello");
+	  }
+	}
+}
