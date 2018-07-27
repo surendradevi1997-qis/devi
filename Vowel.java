8f24c2bbfb231597864e942d1ab69ed6e520d0fb
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-07-27 09:45:16 +0530

    Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Vowel
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch='i';
+		if(ch=='i')
+		{
+			System.out.println(ch+"is a vowel");
+		}
+	else
+	{
+		System.out.println(ch+"is a consonant");
+	}
+
+	}
+}
