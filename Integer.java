8dfd6c1c6c4128ca301ed7ec3d2e3af4c4bb3bbf
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-07-27 10:48:15 +0530

    Integer.java

---
+++ Integer.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Integer
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n[]={1,2,3,4,5},k=2,i;
+	   	int sum=0;
+		for(i=0;i<k;i++)
+		{
+	              sum=sum+n[i];
+		}
+		System.out.println("sum is"+sum);
+	}
+}
