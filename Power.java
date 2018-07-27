c1de886491b0c02f5e33b43670e340127f59227e
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-07-27 11:16:32 +0530

    Power.java

---
+++ Power.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Power
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=2,b=3;
+	            double result=Math.pow(a,b);
+		System.out.println("result" +result);
+	}
+}
