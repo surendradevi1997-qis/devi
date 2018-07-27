d6fb4a9d47a857edfecc2485026887450cb9fe7d
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-07-27 10:06:18 +0530

    Leap.java

---
+++ Leap.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Leap
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+       int y=2000;
+       if(y%4==0)
+       {
+       	System.out.println("y is leap year");
+       }
+       else
+       {
+       	System.out.println("y is not a leap year");
+       }
+	}
+}
