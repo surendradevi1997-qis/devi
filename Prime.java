7ddb14f0febf79b94b86d25aacd61e6faa6633b3
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-07-27 12:23:35 +0530

    Prime.java

---
+++ Prime.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Prime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i=11,n;
+		n=i;
+		if(i%1==0&&i%n==0)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
