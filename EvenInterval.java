65dcab9d756e8010d99d1d3b753fb1c090586c01
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-07-27 13:12:19 +0530

    EvenInterval.java

---
+++ EvenInterval.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class EvenInterval
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+         		for (int i = 1; i <6; i++) {
+			if (i % 2 == 0) {
+				System.out.print(i + " ");
+			}
+		}
+
+	}
+}
