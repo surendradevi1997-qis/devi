61b76a7dad35afe5dccc7529c587ec84a4da30f5
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-07-27 09:35:24 +0530

    Even.java

---
+++ Even.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Even
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int a=10;
+	  if(a%2==0)
+	  {
+	  	System.out.println("a is even");
+	  }
+	  else
+	  {
+	  System.out.println("a is odd");	
+	  }
+	}
+}
