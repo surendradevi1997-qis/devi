ed519e6dd5bbcc4890cefc2b12687888fed3ca65
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-08-01 13:40:23 +0530

    Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Armstrong
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int num = 371, number, temp, total = 0;
+
+        number = num;
+        while (number != 0)
+        {
+            temp = number % 10;
+            total = total + temp*temp*temp;
+            number /= 10;
+        }
+
+        if(total == num)
+            System.out.println(num + "yes");
+        else
+            System.out.println(num + "no");
+	}
+}
