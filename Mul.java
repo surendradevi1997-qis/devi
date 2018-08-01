8dfbe95461c270b3f60f84c0acd09abfc775a584
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-08-01 14:30:10 +0530

    Mul.java

---
+++ Mul.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Mul
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   int i,n=5;
+	   for(i=1;i<=5;i++)
+	   {
+	   	System.out.println((n*i+" "));
+	   }
+	}
+}
