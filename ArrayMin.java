c521276f75b83f5bac8e7f66ed882dd0aaf95c01
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-08-03 13:49:36 +0530

    ArrayMin.java

---
+++ ArrayMin.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ArrayMin
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int n, min;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        int a[] = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            a[i] = s.nextInt();
+        }
+        min = a[0];
+        for(int i = 0; i < n; i++)
+        {
+            if( a[i] < min)
+            {
+                min = a[i];
+            }
+        }
+        System.out.println("Minimum value:"+min);
+	}
+}
