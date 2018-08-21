b39dc13cb5cd8fd60f782b944b20f92129cc1cb6
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-08-21 11:48:23 +0530

    31.java

---
+++ 31.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int count=0,i;
+		Scanner s=new Scanner(System.in);
+		String c=s.nextLine();
+		for(i=0;i<c.length();i++)
+		if(c.charAt(i)!=' ')
+		{
+			count++;
+		}
+		System.out.println(count);
+	}
+}
