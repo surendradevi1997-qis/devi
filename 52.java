c82d59f27e8081571b683773f8cf40c4ef7c710b
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-08-22 12:52:32 +0530

    Update 52.java

--- 52.java
+++ 52.java
@@ -16,37 +16,37 @@ class Ideone
 		switch(n)
 		{
 			case 1:
-				System.out.println("one");
+				    System.out.println("one");
 			break;
 			case 2:
 			            System.out.println("two");
 			break;
 			case 3:
-			System.out.println("three");
+			            System.out.println("three");
 			break;
 			case 4:
-			System.out.println("four");
+			            System.out.println("four");
 			break;
 			case 5:
-			System.out.println("five");
+			            System.out.println("five");
 			break;
 			case 6:
-			System.out.println("six");
+			            System.out.println("six");
 			break;
 			case 7:
-			System.out.println("seven");
+			            System.out.println("seven");
 			break;
 			case 8:
-			System.out.println("eight");
+			            System.out.println("eight");
 			break;
 			case 9:
-			System.out.println("nine");
+			            System.out.println("nine");
 			break;
 			case 10:
-				System.out.println("ten");
+				   System.out.println("ten");
 			break;
 			case 11:
-				System.out.println(" ");
+				   System.out.println(" ");
 		}	
 	}
 }
