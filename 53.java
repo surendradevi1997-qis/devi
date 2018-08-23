0c5f3031c2fad35db6bb5b171aa0e027883dc323
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-08-23 09:33:05 +0530

    Create 53.java

---
+++ 53.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,sum=0,m;
+		Scanner s=new Scanner(System.in);
+		m=s.nextInt();
+		while(m > 0)
+        {
+            n = m % 10;
+            sum = sum + n;
+            m = m / 10;
+        }
+        System.out.println(sum);
+	}
+}
