7e336e5427c4ce6ad4360b96497346df3b9013ce
surendradevi1997-qis <41726196+surendradevi1997-qis@users.noreply.github.com>
2018-09-29 16:29:08 +0530

    Create Player1.java

---
+++ Player1.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String reverse="";
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			reverse=reverse+s.charAt(i);
+		}
+		System.out.println(reverse);
+	}
+}
